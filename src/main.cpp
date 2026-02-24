d41ab81227a213c002ed51c6d675eb42ee525133
xColtonGMDx <257379097+xColtonGMDx@users.noreply.github.com>
2026-02-23 23:04:41 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include <Geode/loader/AlertLayer.hpp>
 
 using namespace geode::prelude;
 
@@ -11,7 +10,7 @@ public:
             return false;
 
         // Find the title sprite
-        auto title = this->getChildByID("game-title");
+        auto title = this->getChildByID("main-menu");
         if (!title)
             return true;
 
@@ -25,10 +24,8 @@ public:
             menu_selector(HiButtonMenuLayer::onHiButton)
         );
 
-        // Position the button next to the title
-        auto pos = title->getPosition();
-        float offset = title->getContentSize().width * title->getScale() / 2 + 50.f;
-        btn->setPosition(pos.x + offset, pos.y);
+        // --- REPLACED CHUNK: guaranteed visible position ---
+        btn->setPosition({200, 200});
 
         // Add to menu
         auto menu = CCMenu::create();
@@ -40,6 +37,36 @@ public:
     }
 
     void onHiButton(CCObject*) {
-        FLAlertLayer::create("Hi", "hi", "OK")->show();
+        // --- Android-safe popup replacement ---
+        auto layer = CCLayerColor::create({0, 0, 0, 180});
+
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+
+        auto bg = CCSprite::create("GJ_square01.png");
+        bg->setScale(2.0f);
+        bg->setPosition(winSize / 2);
+        layer->addChild(bg);
+
+        auto label = CCLabelBMFont::create("hi :) im useless", "bigFont.fnt");
+        label->setPosition(winSize / 2);
+        layer->addChild(label);
+
+        auto closeBtn = CCMenuItemSpriteExtra::create(
+            CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png"),
+            layer,
+            [](CCObject*) {
+                auto scene = CCDirector::sharedDirector()->getRunningScene();
+                scene->removeChildByTag(999);
+            }
+        );
+
+        auto menu = CCMenu::create();
+        menu->addChild(closeBtn);
+        menu->setPosition(winSize.width / 2 + 120, winSize.height / 2 + 120);
+        layer->addChild(menu);
+
+        layer->setTag(999);
+
+        CCDirector::sharedDirector()->getRunningScene()->addChild(layer, 999);
     }
 };
